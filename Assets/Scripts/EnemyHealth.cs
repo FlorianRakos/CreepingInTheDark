5d20335492305d538b2172822cdc042eeaf09c91
Florian167 <florian.rakos@yahoo.com>
2020-06-06 00:31:00 +0200

    02 Crosshair, Gun, Muzzleflash, Raycasting, Enemy Health

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    // public method
+    public void decreaseHealth (float decrease) {
+        hitPoints -= decrease;
+
+        if (hitPoints <= 0) {
+            KillEnemy();
+        }
+
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float shootingRange = 100f;
+    [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
+
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        PlayMuzzleFlash();
+        PlayShootingAnimation();
+        ProcessRaycast();
+
+
+    }
+
+    private void PlayShootingAnimation()
+    {
+        Animation  animation = GetComponent<Animation>();
+        animation.Play();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, shootingRange))
+        {
+            print(hit.transform.name);
+            // todo: add some hit FX
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target != null)
+            {
+                target.decreaseHealth(damage);
+
+            }
+
+
+
+        }
+        else
+        {
+            return;
+        }
+    }
+}
