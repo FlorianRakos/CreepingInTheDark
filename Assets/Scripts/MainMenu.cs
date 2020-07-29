2d9b8652dc09d9b8d9af16a862e09ed1873b4a7f
Florian167 <florian.rakos@yahoo.com>
2020-07-29 18:50:14 +0200

    18 Menu, URP

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{   
+    [SerializeField] Canvas mainCanvas, storyCanvas, controllCanvas, mainBackground, storyBackground;
+    
+
+    void Start()
+    {
+        showMainMenu();
+
+    }
+
+    public void showMainMenu () {
+        disableAll();
+        mainCanvas.enabled = true;
+        mainBackground.enabled = true;
+    }
+
+    public void showStoryMenu () {
+        disableAll();
+        storyCanvas.enabled = true;
+        storyBackground.enabled = true;
+    }
+
+    public void showControllMenu () {
+        disableAll();
+        controllCanvas.enabled = true;
+        storyBackground.enabled = true;
+    }
+
+    private void disableAll () {
+        mainCanvas.enabled = false;
+        storyCanvas.enabled = false;
+        controllCanvas.enabled = false;
+        mainBackground.enabled = false;
+        storyBackground.enabled = false;
+    }
+    
+}
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-
+    
 
     public void LoadGame() {
         SceneManager.LoadScene(1);
